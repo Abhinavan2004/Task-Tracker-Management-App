d63f00d990046401537dfd4d60bcef7ae6916a9b
Abhinavan2004 <abhinavanpan@gmail.com>
2026-01-03 17:15:46 +0530

    Added intial files

---
+++ src/main/java/com/Task_Tracker_App/Task_Tracker_App/TaskTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.Task_Tracker_App.Task_Tracker_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerAppApplication.class, args);
+	}
+
+}
